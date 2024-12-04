a86878b9cac66b2299514db846a0c212041109bd
Cord Franks <crfranks94@gmail.com>
2024-12-04 11:15:28 -0600

    sign-in screen/ add oauth component

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,5 +1,5 @@
 import { InputFieldProps } from "@/types/type";
-import { KeyboardAvoidingView, Text, TouchableWithoutFeedback, View, Image, TextInput, Platform } from "react-native";
+import { KeyboardAvoidingView, Text, TouchableWithoutFeedback, View, Image, TextInput, Platform, Keyboard } from "react-native";
 
 const InputField = ({ 
     label, 
@@ -12,17 +12,23 @@ const InputField = ({
     className, 
     ...props 
 }: InputFieldProps) => (
-        <View className="my-2 w-full">
-            <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>{label}</Text>
-            <View className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle}`}>
-                {icon && <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />}
-                <TextInput 
-                    className={`rounded-full p-4 font-JakartaSemiBold text-[15px] flex-1 text-left ${inputStyle}`}
-                    secureTextEntry={secureTextEntry}
-                    {...props}
-                />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View className="my-2 w-full">
+                <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>{label}</Text>
+                <View className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle}`}>
+                    {icon && <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />}
+                    <TextInput 
+                        className={`rounded-full p-4 font-JakartaSemiBold text-[15px] flex-1 text-left ${inputStyle}`}
+                        secureTextEntry={secureTextEntry}
+                        {...props}
+                    />
+                </View>
             </View>
-        </View>
+        </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
 )
 
 export default InputField;
