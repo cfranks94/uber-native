7806a06619bb063adc00f6b3a8572e2974c4f65e
Cord Franks <crfranks94@gmail.com>
2025-02-05 09:36:58 -0600

    add Confirm Ride screen and update Find Ride layout; integrate BottomSheet for improved UX

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,13 +1,42 @@
+import CustomButton from "@/components/CustomButton";
+import GoogleTextInput from "@/components/GoogleTextInput";
 import RideLayout from "@/components/RideLayout";
+import { icons } from "@/constants";
 import { useLocationStore } from "@/store";
+import { router } from "expo-router";
 import { Text, View } from "react-native";
 
 const FindRide = () => {
     const { userAddress, destinationAddress, setDestinationLocation, setUserLocation } = useLocationStore();
 
     return (
-        <RideLayout>
-            <Text className="text-2xl ">Find Ride</Text>
+        <RideLayout title="Ride" snapPoints={['85%']}>
+            <View className="my-3">
+                <Text className="text-lg font-JakartaSemiBold mb-3">From</Text>
+                <GoogleTextInput 
+                    icon={icons.target}
+                    initialLocation={userAddress!}
+                    containerStyle="bg-neutral-100"
+                    textInputBackgroundColor="#f5f5f5"
+                    handlePress={(location) => setUserLocation(location)}
+                />
+            </View>
+            <View className="my-3">
+                <Text className="text-lg font-JakartaSemiBold mb-3">To</Text>
+                <GoogleTextInput 
+                    icon={icons.map}
+                    initialLocation={destinationAddress!}
+                    containerStyle="bg-neutral-100"
+                    textInputBackgroundColor="transparent"
+                    handlePress={(location) => setDestinationLocation(location)}
+                />
+            </View>
+
+            <CustomButton 
+                title="Find now"
+                className="mt-5"
+                onPress={() => router.push('/(root)/confirm-ride')}
+            />
         </RideLayout>
     )
 }
--- components/RideLayout.tsx
+++ components/RideLayout.tsx
@@ -2,9 +2,13 @@ import { icons } from "@/constants";
 import { router } from "expo-router";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import Map from "./Map";
+import { useRef } from "react";
 
-const RideLayout = ({ children, title }: { children: React.ReactNode, title: string }) => {
+const RideLayout = ({ children, title, snapPoints }: { children: React.ReactNode, title: string, snapPoints: string[] }) => {
+    const bottomSheetRef = useRef<BottomSheet>(null);
+
     return (
         <GestureHandlerRootView>
             <View className="flex-1 bg-white">
@@ -23,6 +27,16 @@ const RideLayout = ({ children, title }: { children: React.ReactNode, title: str
                     </View>
                     <Map />
                 </View>
+
+                <BottomSheet 
+                    ref={bottomSheetRef}
+                    snapPoints={snapPoints || ['40%', '85%']}
+                    index={0}
+                >
+                    <BottomSheetView style={{ flex: 1, padding: 20 }}>
+                        {children}
+                    </BottomSheetView>
+                </BottomSheet>
             </View>
         </GestureHandlerRootView>
     )
